3e2bf5e708c0f65bd1835a56c15101fd4fc4230d
SachinGadge13 <gadges14@gmail.com>
2022-10-14 22:53:33 +0530

    Added:Program for user to Enter a valid Email Address

--- src/com/bridgelabz/JUnitTesting/App.java
+++ src/com/bridgelabz/JUnitTesting/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Sachin Gadge!" );
+        System.out.println( "Hello World!" );
     }
 }
--- src/com/bridgelabz/JUnitTesting/UserRegistrationTest.java
+++ src/com/bridgelabz/JUnitTesting/UserRegistrationTest.java
@@ -11,9 +11,9 @@ public class UserRegistrationTest {
 	public void before() {
 		userRegistration = new UserRegistration();
 	}
-    /**
-     * Unit test for validating first name
-     */
+	/**
+	 * Unit test for validating first name
+	 */
 
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
@@ -23,7 +23,7 @@ public class UserRegistrationTest {
 
 	@Test
 	public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
-		boolean result = userRegistration.firstName("utkarsh");
+		boolean result = userRegistration.firstName("Utkarsh");
 		Assert.assertEquals(false, result);
 	}
 	/**
@@ -31,7 +31,7 @@ public class UserRegistrationTest {
 	 */
 	@Test
 	public void givenLastName_WhenProper_ShouldReturnTrue() {
-		boolean result = userRegistration.lastName("Mishra");
+		boolean result = userRegistration.lastName("mishra");
 		Assert.assertEquals(true, result);
 	}
 
@@ -40,6 +40,20 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.lastName("mishra");
 		Assert.assertEquals(false, result);
 
+	}
+	/**
+	 * Unit test for validating email address
+	 */
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.email("abc@gmail.com.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.email("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
 	}
 
 }
