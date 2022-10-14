a58535c19ecbfeae0cee2a2a390d37449a507ae0
SachinGadge13 <gadges14@gmail.com>
2022-10-14 22:47:24 +0530

    Added:Program for user to Enter a valid Last Name

--- src/com/bridgelabz/JUnitTesting/UserRegistration.java
+++ src/com/bridgelabz/JUnitTesting/UserRegistration.java
@@ -2,9 +2,8 @@ package com.bridgelabz.JUnitTesting;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 /**
- * Purpose - As a User need to enter a valid First Name
+ * Purpose  - As a User need to enter a valid Last Name
  */
 
 public class UserRegistration {
@@ -13,6 +12,15 @@ public class UserRegistration {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
+	/*
+    Created a method to validate last name
+	 */
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
 		return matcher.matches();
 	}
 
--- src/com/bridgelabz/JUnitTesting/UserRegistrationTest.java
+++ src/com/bridgelabz/JUnitTesting/UserRegistrationTest.java
@@ -5,27 +5,41 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for validating first name
- */
-
 public class UserRegistrationTest {
-	    UserRegistration userRegistration;
-	    @Before
-	    public void before() {
-	        userRegistration = new UserRegistration();
-	    }
+	UserRegistration userRegistration;
+	@Before
+	public void before() {
+		userRegistration = new UserRegistration();
+	}
+    /**
+     * Unit test for validating first name
+     */
 
-	    @Test
-	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Utkarsh");
-	        Assert.assertEquals(true, result);
-	    }
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Utkarsh");
+		Assert.assertEquals(true, result);
+	}
 
-	    @Test
-	    public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
-	        boolean result = userRegistration.firstName("utkarsh");
-	        Assert.assertEquals(false, result);
-	    }
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("utkarsh");
+		Assert.assertEquals(false, result);
+	}
+	/**
+	 * Unit test for validating last name
+	 */
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Mishra");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("mishra");
+		Assert.assertEquals(false, result);
+
+	}
 
 }
